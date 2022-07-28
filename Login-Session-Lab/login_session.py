d5093422670439d24b58a8d499859eb0d9a5832d
lynnefa23-meet <st.lynne.al-faqih@rosaryshs-j.com>
2022-07-28 16:15:00 +0300

    done!!!!

--- Login-Session-Lab/login_session.py
+++ Login-Session-Lab/login_session.py
@@ -6,9 +6,21 @@ app.config['SECRET_KEY'] = 'super-secret-key'
 
 @app.route('/', methods = ["POST", "GET"]) # What methods are needed?
 def home():
-	 # if request.method == "GET"
-	return render_template('home.html')
+	if request.method == "POST":
+		try:
+			name= request.form["author's name"]
+			age= request.form["author's age"]
+			quote= request.form["enter a quote"]
+			login_session["author's name"]=name
+			login_session["author's age"]=age
+			login_session["enter a quote"]=quote
+			return render_template("thanks.html", quote=login_session["enter a quote"], name=login_session["author's name"], age=login_session["author's age"])
+		except: 
+			return render_template("error.html")
 
+	else: 
+		return render_template('home.html')
+		
 
 @app.route('/error')
 def error():
@@ -19,7 +31,7 @@ def error():
 @app.route('/display')
 def display():
 
-	return render_template('display.html', ) # What variables are needed?
+	return render_template('display.html', quote=login_session["enter a quote"], name=login_session["author's name"], age=login_session["author's age"]) # What variables are needed?
 
 
 @app.route('/thanks')
